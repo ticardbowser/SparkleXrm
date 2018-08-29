882553d481a391b9e0008b2c55c407e2ade162f1
Ticard Bowser <ticardb@gmail.com>
2018-08-29 10:40:18 -0700

    Update to remove requirement of OrganizationServiceProxy and work with OrganizationWebProxyClient if available

--- spkl/spkl/Program.cs
+++ spkl/spkl/Program.cs
@@ -176,18 +176,23 @@ namespace SparkleXrmTask
 
                     using (var serviceProxy = new CrmServiceClient(arguments.Connection))
                     {
-                        if (serviceProxy.OrganizationServiceProxy == null)
+                        /*if (serviceProxy.OrganizationServiceProxy == null)
                         {
                             throw new SparkleTaskException(SparkleTaskException.ExceptionTypes.AUTH_ERROR, String.Format("Error connecting to the Organization Service Proxy: {0}", serviceProxy.LastCrmError));
-                        }
+                        }*/
 
-                        serviceProxy.OrganizationServiceProxy.Timeout = new TimeSpan(1, 0, 0);
+                        //serviceProxy.OrganizationServiceProxy.Timeout = new TimeSpan(1, 0, 0);
                         if (!serviceProxy.IsReady)
                         {
                             trace.WriteLine("Not Ready {0} {1}", serviceProxy.LastCrmError, serviceProxy.LastCrmException);
                         }
+                        else
+                        {
+                            IOrganizationService crmService = (IOrganizationService)serviceProxy.OrganizationWebProxyClient != null ? (IOrganizationService)serviceProxy.OrganizationWebProxyClient : (IOrganizationService)serviceProxy.OrganizationServiceProxy;
+                            RunTask(arguments, crmService, trace);
+                        }
 
-                        RunTask(arguments, serviceProxy, trace);
+                        
                     }
                 }
             }
